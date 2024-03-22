ea73cbca443a134116400a3fadfc9b1a5857f107
I.Devescovi <igor.devescovi@gmail.com>
2024-03-22 13:49:16 +0100

    first import

---
+++ px3-auth/src/main/java/com/id/px3/auth/Px3AuthApplication.java
@@ -0,0 +1,13 @@
+package com.id.px3.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Px3AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Px3AuthApplication.class, args);
+	}
+
+}
---
+++ px3-auth/src/test/java/com/id/px3auth/Px3AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.px3auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Px3AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
