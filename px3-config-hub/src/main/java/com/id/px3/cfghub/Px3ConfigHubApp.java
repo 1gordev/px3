25141e6f6827f1ff9cca76fb4d933e2e18d2324b
I.Devescovi <igor.devescovi@gmail.com>
2025-06-02 09:27:03 +0200

    config hub

---
+++ px3-config-hub/src/main/java/com/id/px3/cfghub/Px3ConfigHubApp.java
@@ -0,0 +1,13 @@
+package com.id.px3.cfghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Px3ConfigHubApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Px3ConfigHubApp.class, args);
+	}
+
+}
---
+++ px3-config-hub/src/test/java/com/id/px3/cfghub/Px3ConfigHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.px3.cfghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Px3ConfigHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
